ca0689b8cd60b970a0ea1f6c87ba0705ef36977a
Arshin Jain <arshin.jain@outlook.com>
2016-05-30 16:13:32 -0700

    Updated initializer in WeatherIcon model

--- Stormy/WeatherIcon.swift
+++ Stormy/WeatherIcon.swift
@@ -24,7 +24,19 @@ enum WeatherIcon: String {
     case UnexpectedType = "default" // Added to prevent unexpected results
     
     init(rawValue: String) {
-        
+        switch rawValue {
+        case "clear-day": self = .ClearDay
+        case "clear-night": self = .ClearNight
+        case "rain": self = .Rain
+        case "snow": self = .Snow
+        case "sleet": self = .Sleet
+        case "wind": self = .Wind
+        case "fog": self = .Fog
+        case "cloudy": self = .Cloudy
+        case "partly-cloudy-day": self = .PartlyCloudyDay
+        case "partly-cloudy-night": self = .PartlyCloudyNight
+        default: self = .UnexpectedType
+        }
     }
     
 }
