b87cede64fb770c5f712cfdf766586fb44a5481d
Arshin Jain <arshin.jain@outlook.com>
2016-05-30 16:08:07 -0700

    WeatherIcon Model Updated with numerous possible cases

--- Stormy/WeatherIcon.swift
+++ Stormy/WeatherIcon.swift
@@ -10,4 +10,16 @@ import Foundation
 
 enum WeatherIcon: String {
     
+    case ClearDay = "clear-day"
+    case ClearNight = "clear-night"
+    case Rain = "rain"
+    case Snow = "snow"
+    case Sleet = "sleet"
+    case Wind = "wind"
+    case Fog = "fog"
+    case Cloudy = "cloudy"
+    case PartlyCloudyDay = "partly-cloudy-day"
+    case PartlyCloudyNight = "partly-cloudy-night"
+    case UnexpectedType = "default"
+    
 }
