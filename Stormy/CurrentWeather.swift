b807ba0d18ff9e9c5326170c23eaae29e5f58017
Arshin Jain <arshin.jain@outlook.com>
2016-05-30 16:05:26 -0700

    CurrentWeather model added and grouped

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,19 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Arshin Jain on 5/30/16.
+//  Copyright © 2016 Arshin Jain. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeather {
+    
+    let temperature: Double
+    let humidity: Double
+    let precipitationProperty: Double
+    let summary: String
+    let icon: UIImage
+}
